e3805093257fc4c07c58bdf0f8d814267fa6cf79
Greg Sjaardema <gsjaardema@gmail.com>
2016-06-01 09:57:40 -0600

    CONFIG: Better netcdf and hdf5 detection

--- packages/seacas/cmake/Dependencies.cmake
+++ packages/seacas/cmake/Dependencies.cmake
@@ -2,6 +2,7 @@ TRIBITS_PACKAGE_DEFINE_DEPENDENCIES(
   SUBPACKAGES_DIRS_CLASSIFICATIONS_OPTREQS
   Exodus      libraries/exodus        SS  REQUIRED
   Exodus_for  libraries/exodus_for    SS  REQUIRED
+  ExoIIv2for32 libraries/exoIIv2for32 SS  REQUIRED
 )
 
 SET(LIB_OPTIONAL_DEP_TPLS MPI)
